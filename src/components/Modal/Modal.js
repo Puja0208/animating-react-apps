8cd3853762bc2e4bff8cfb1f0662649d5d846ed0
puja.rani <puja.rani@ril.com>
2022-03-15 07:53:35 +0530

    feat: Use CSSTransition component to move through css classes automatically

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Transition from "react-transition-group/Transition";
+import CSSTransition from "react-transition-group/CSSTransition";
 import "./Modal.css";
 
 const animationTiming = {
@@ -17,31 +17,20 @@ const modal = (props) => {
       : null,
   ];
   return (
-    <Transition
+    <CSSTransition
       in={props.show}
       timeout={animationTiming}
       mountOnEnter
       unmountOnExit
+      classNames="fade-slide"
     >
-      {(state) => {
-        const cssClasses = [
-          "Modal",
-          state === "entering"
-            ? "ModalOpen"
-            : state === "exiting"
-            ? "ModalClose"
-            : null,
-        ];
-        return (
-          <div className={cssClasses.join(" ")}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
-      }}
-    </Transition>
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 

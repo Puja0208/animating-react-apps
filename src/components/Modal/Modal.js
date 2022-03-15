3d1c760a500af4915e33f82d838dce0c3479ee23
puja.rani <puja.rani@ril.com>
2022-03-15 07:41:36 +0530

    feat: Move transition inside modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Transition from "react-transition-group/Transition";
 import "./Modal.css";
 
 const modal = (props) => {
@@ -12,12 +12,26 @@ const modal = (props) => {
       : null,
   ];
   return (
-    <div className={cssClasses.join(" ")}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+      {(state) => {
+        const cssClasses = [
+          "Modal",
+          state === "entering"
+            ? "ModalOpen"
+            : state === "exiting"
+            ? "ModalClose"
+            : null,
+        ];
+        return (
+          <div className={cssClasses.join(" ")}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
   );
 };
 

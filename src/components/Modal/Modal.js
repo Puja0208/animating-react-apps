9038e419395b70982d15707d9708409b12deccd0
puja.rani <puja.rani@ril.com>
2022-03-15 07:44:15 +0530

    feat: Define different entering & exit time

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,6 +2,11 @@ import React from "react";
 import Transition from "react-transition-group/Transition";
 import "./Modal.css";
 
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
 const modal = (props) => {
   const cssClasses = [
     "Modal",
@@ -12,7 +17,12 @@ const modal = (props) => {
       : null,
   ];
   return (
-    <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+    <Transition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+    >
       {(state) => {
         const cssClasses = [
           "Modal",
